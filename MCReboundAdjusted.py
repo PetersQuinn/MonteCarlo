c10513398dda26b253b979d4ce637365461ca8c5
Petersquinn <quinton.peters@duke.edu>
2025-04-02 12:39:59 -0400

    Started commits to MCReboundAdjusted.py

--- MCReboundAdjusted.py
+++ MCReboundAdjusted.py
@@ -1,1 +1,9 @@
 #imports
+import yfinance as yf
+import pandas as pd
+import numpy as np
+from pandas_datareader import data as pdr
+import datetime
+
+'''Initially, we have to determine which factors tend to affect our test stock, especially over recent times. 
+This will allow us to build the rebound model for testing.'''
